1037f9830a4cde3249cdb690eecb4059bf2b3fbb
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2025-02-02 18:03:12 +0700

    feat: implementation realtime chat with websocket

--- fe-next-js/src/app/page.tsx
+++ fe-next-js/src/app/page.tsx
@@ -1,101 +1,78 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import Pusher from "pusher-js";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<string[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    if (!message) return;
+
+    await axios.get(
+      `http://127.0.0.1:8000/api/message?message=${encodeURIComponent(message)}`
+    );
+
+    setMessage("");
+    console.log("Message sent:", message);
+  };
+
+  useEffect(() => {
+    const pusher = new Pusher("my_key", {
+      cluster: "mt1",
+      wsHost: "127.0.0.1",
+      wsPort: 6001,
+      forceTLS: false,
+      disableStats: true,
+    });
+
+    const channel = pusher.subscribe("chat");
+    channel.bind("chat-message", (data: any) => {
+      setMessages((prevMessages) => [...prevMessages, data.message]);
+      console.log("messages");
+      console.log(messages);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <div className="flex items-start justify-center h-screen">
+      <div className="border border-black w-1/2 p-3 flex flex-col gap-3">
+        {messages.map((m, index) => (
+          <div
+            key={index}
+            className="py-2 px-4 bg-orange-300 rounded-md inline-block ml-auto text-black"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            {m}
+          </div>
+        ))}
+
+        <div className="flex gap-3">
+          <input
+            type="text"
+            name="message"
+            id="message"
+            className="w-full rounded-md bg-slate-200 border-0 text-black p-2"
+            onChange={handleInputChange}
+            value={message}
+          />
+          <button
+            onClick={handleSubmit}
+            className="py-2 px-4 bg-green-500  rounded-lg text-white"
           >
-            Read our docs
-          </a>
+            Send
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\MessageSent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/hay', function (Request $request) {
+    return response()->json([
+        'message' => 'Welcome to api'
+    ]);
+});
+
+Route::get('/message', function (Request $request) {
+    broadcast(new MessageSent($request->message ?? ''))->toOthers();
+
+    return response()->json([
+        'message' => $request->message ?? '',
+        'status' => 200,
+    ], 200);
 });
